d574e825fe211bdf1348f31c675173fc6a5100cf
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-24 13:53:15 +0200

    Add delete, HATEOAS Links in Response

--- src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -1,14 +1,20 @@
 package com.ltp.gradesubmission.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 import com.ltp.gradesubmission.entity.Student;
 import com.ltp.gradesubmission.service.StudentService;
@@ -26,8 +32,27 @@ public class StudentController {
 
     @PostMapping
     public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        
+        Link selfLink = WebMvcLinkBuilder.linkTo(
+            WebMvcLinkBuilder
+                .methodOn(StudentController.class)
+                .getStudent(student.getId()))
+                .withRel("self");
+        Link deleteLink = WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder
+                    .methodOn(StudentController.class)
+                    .deleteStudent(student.getId()))
+                    .withRel("delete");
+        student.add(selfLink);
+        student.add(deleteLink);
 
         return new ResponseEntity<Student>(student,HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id){
+        //TODO Return the deleted student for cross-checking
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
     }
 
 }
