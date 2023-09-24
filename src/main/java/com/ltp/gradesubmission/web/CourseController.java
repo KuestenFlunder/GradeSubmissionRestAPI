96e8a19efce137dfbc3b15a7ca0ee430f10b4361
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-25 00:06:37 +0200

    fix unused imports

--- src/main/java/com/ltp/gradesubmission/web/CourseController.java
+++ src/main/java/com/ltp/gradesubmission/web/CourseController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ltp.gradesubmission.entity.Course;
-import com.ltp.gradesubmission.entity.Grade;
 
 
 @RestController
--- src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -3,25 +3,16 @@ package com.ltp.gradesubmission.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ltp.gradesubmission.entity.Course;
 import com.ltp.gradesubmission.entity.Grade;
-import com.ltp.gradesubmission.entity.Student;
-
 import java.util.List;
 
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 @RequestMapping("/grades")
