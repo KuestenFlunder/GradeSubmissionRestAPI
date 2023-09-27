17489b3d36215deea6c56f519beb7cb08c454c6b
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-27 21:08:23 +0200

    add/validation constrains

--- src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.hateoas.RepresentationModel;
@@ -24,9 +26,11 @@ public class Student extends RepresentationModel<Student>{
     private Long id;
     @NonNull
     @Column(name = "name",nullable = false)
+    @NotBlank(message = "Name can not be blank")
     private String name;
     @NonNull
     @Column(name = "birth_date",nullable = false)
+    @Past
     private LocalDate birthDate;
 
     @JsonIgnore //to omit infinite loop between Grade and Student while JSON serialisation
