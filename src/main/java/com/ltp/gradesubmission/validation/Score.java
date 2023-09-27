b1cf3c7f806522acc7ae2a69a1467af0987f6f44
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-27 14:30:30 +0200

    add/ScoreValidation

--- src/main/java/com/ltp/gradesubmission/validation/Score.java
+++ src/main/java/com/ltp/gradesubmission/validation/Score.java
@@ -6,7 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
-import javax.validation.Payload;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = ScoreValidator.class)
