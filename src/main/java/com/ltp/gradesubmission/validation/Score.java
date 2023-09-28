09074975b769798367cd488e6daebeb89d39879c
KuestenFlunder <129527120+KuestenFlunder@users.noreply.github.com>
2023-09-28 11:00:53 +0200

    hotfix/ add missing score validation error message

--- src/main/java/com/ltp/gradesubmission/validation/Score.java
+++ src/main/java/com/ltp/gradesubmission/validation/Score.java
@@ -6,12 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
+
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = ScoreValidator.class)
 public @interface Score {
-
-
-
-
+String message() default "The score is not valid";
+Class<?>[] groups() default {};
+Class<? extends Payload>[] payload() default {};
 }
